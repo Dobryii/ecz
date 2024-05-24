4f739ba5ada6a142a13c1dd747dfc939ca70291a
Dobryii <137163843+Dobryii@users.noreply.github.com>
2024-05-24 16:51:24 +0300

    Add files via upload

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            {
+                string filePath = "C:\\Users\\Student\\Desktop\\1.txt";
+                Console.WriteLine("Введите текст для записи в файл:");
+                string inputText = Console.ReadLine();
+                using (StreamWriter writer = File.AppendText(filePath))
+                {
+                    writer.WriteLine(inputText);
+                }
+                Console.WriteLine("Текст успешно записан в файл.");
+            }
+        }
+    }
+
+}
+   
